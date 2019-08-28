508dfffc58cdc56469aef1af3a2f134fdf396b53
Carmen <sdluojm@gmail.com>
2019-08-28 17:56:02 +0000

    handle ctrl+C

--- play.js
+++ play.js
@@ -2,5 +2,22 @@ const net = require('net');
 
 const {connect} = require('./client');
 
+const setupInput = function (){
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+
+  stdin.resume();
+  handleUserInput(stdin);
+  return stdin;
+}
+function handleUserInput(stdin){
+  stdin.on('data',(key)=>{
+    if (key === '\u0003'){
+      process.exit();
+    }
+  })
+}
 console.log('connecting...');
+setupInput();
 connect();
