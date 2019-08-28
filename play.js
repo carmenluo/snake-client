28d7a9709055f4ce89294dda7c29c84ab9b89459
Carmen <sdluojm@gmail.com>
2019-08-28 18:13:51 +0000

    modulize input

--- play.js
+++ play.js
@@ -1,23 +1,8 @@
 const net = require('net');
 
 const {connect} = require('./client');
+const {setupInput} = require('./input');
 
-const setupInput = function (){
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding('utf8');
-
-  stdin.resume();
-  handleUserInput(stdin);
-  return stdin;
-}
-function handleUserInput(stdin){
-  stdin.on('data',(key)=>{
-    if (key === '\u0003'){
-      process.exit();
-    }
-  })
-}
 console.log('connecting...');
 setupInput();
 connect();
