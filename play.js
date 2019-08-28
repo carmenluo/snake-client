7419c7b1f6224c118e90b18c0794c34c6cf1769e
Carmen <sdluojm@gmail.com>
2019-08-28 16:33:24 +0000

    get message from server

--- play.js
+++ play.js
@@ -1,12 +1,14 @@
 const net = require('net');
 
 const connect = function() {
-  const conn =net.creatConnect({
-    host: '',
+  const conn = net.createConnection({
+    host: 'localhost',
     port: 50541
   });
   conn.setEncoding('utf8');
+  conn.on('data',(data)=>{
+    console.log(data);
   return conn;
-}
+})}
 console.log('connecting...');
 connect();
